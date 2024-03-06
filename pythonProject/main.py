ff16787d8f62f597e69094feee588039e75fb948
step <bodrugtudor8@gamil.com>
2024-03-06 17:16:47 +0200

    class lesson

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -391,4 +391,153 @@
 #     if not tudor.live(day):
 #         break
 
+# class Human:
+#     height = 170
+# class Student(Human):
+#     pass
+# class Worker(Human):
+#     pass
+#
+# nick = Student()
+# kate = Worker()
+# print(f"Nick height is {nick.height}")
+# print(f"Kate height is {kate.height}")
 
+# class Grandparent:
+#     height = 170
+#     satiety = 100
+#     age = 60
+# class Parent(Grandparent):
+#     age = 40
+# class Child(Parent):
+#     height = 50
+#     def __init__(self):
+#         print(self.height)
+#         print(self.satiety)
+#         age = 15
+#         print(age)
+# nick = Child()
+
+# class Hello_World:
+#     hello = "a"
+#     _hello = "b"
+#     __hello = "c"
+#
+#     def __init__(self):
+#         self.world = "1"
+#         self._world = "2"
+#         self.__world = "3"
+#
+#     def printer(self):
+#         print(self.hello)
+#         print(self._hello)
+#         print(self.__hello)
+#         print(self.world)
+#         print(self._world)
+#         print(self.__world)
+#
+# class Hi(Hello_World):
+#     def hi_print(self):
+#         print(self.hello)
+#         print(self.world)
+#         print(self._hello)
+#         print(self._world)
+#         print(self.__hello)
+#         print(self.__world)
+#
+# hello = Hello_World()
+# hello.printer()
+# hi = Hi()
+# hi.hi_print()
+
+# class Hello:
+#     def __init__(self):
+#         print("Hello!")
+# class Hello_World(Hello):
+#     def __init__(self):
+#         super().__init__()
+#         print("World!")
+# hello_world = Hello_World()
+
+# class Class1():
+#     var = 20
+#
+#     def __init__(self):
+#         self.var = 10
+#
+# class Class2(Class1):
+#     def __init__(self):
+#         print(self.var)
+#         super().__init__()
+#         print(self.var)
+#         print(super().var)
+#
+# hello_world = Class2()
+
+# class Grandparent:
+#     def about(self):
+#         print("I am Grandpa")
+#     def about_myself(self):
+#         print("I am Grandpa")
+# class Parent(Grandparent):
+#     def about_myself(self):
+#         print("i am parent")
+# class Child(Parent):
+#     def __init__(self):
+#         super().about()
+#         super().about_myself()
+#
+# nick = Child()
+
+# class Computer:
+#     def calculate(self):
+#         print("Calculating...")
+#
+# class Display:
+#     def display(self):
+#         print("I display the image on the screen")
+#
+# class SmartPhone(Display, Computer):
+#     pass
+#
+# iphone = SmartPhone()
+# iphone.calculate()
+# iphone.display()
+
+# class Computer:
+#     def __init__(self):
+#         super().__init__()
+#         self.memory = 128
+#
+# class Display:
+#     def __init__(self):
+#         super().__init__()
+#         self.resolution = "4k"
+#
+# class SmartPhone(Display, Computer):
+#     def print_info(self):
+#         print(self.resolution)
+#         print(self.memory)
+#
+# iphone = SmartPhone()
+# iphone.print_info()
+
+# class Computer:
+#     def __init__(self, model, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.model  =  model
+#         self.memory = 128
+#
+# class Display:
+#     def  __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.resolution = "4K"
+#
+# class SmartPhone(Display, Computer):
+#     def print_info(self):
+#         print(self.model)
+#         print(self.resolution)
+#         print(self.memory)
+#
+# iphone = SmartPhone(model = "Last")
+# iphone.print_info()
